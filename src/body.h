468c86cba30902904f9adc5a81346cd3d53a2395
drkgrkn <dorukgurkan1@gmail.com>
2025-11-12 22:33:38 +0100

    added the main Body struct

---
+++ src/body.h
@@ -0,0 +1,20 @@
+//
+// Created by Doruk Gürkan on 12.11.2025.
+//
+
+#ifndef NBODY_BODY_H
+#define NBODY_BODY_H
+#include <cmath>
+
+struct Vec3 {
+    double_t x, y, z;
+};
+
+struct Body {
+    uint32_t id;
+    double_t mass;
+    Vec3 pos;
+    Vec3 vel;
+};
+
+#endif  // NBODY_BODY_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "body.h"
+
 int main(int argc, char **argv) {
     const std::string hello_world = "Hello World!";
     std::cout << hello_world << std::endl;
