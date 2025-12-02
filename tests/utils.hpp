61accf73c94773f443851ec2c13f2c08bd1e8f41
shadymeowy <tolgademirdal@gmail.com>
2025-12-02 22:12:42 +0100

    add tests

---
+++ tests/utils.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstdlib>
+
+#include "common/vec.hpp"
+
+// ref: good old numpy's isclose
+// https://numpy.rg/doc/stable/reference/generated/numpy.isclose.html
+inline auto isclose(double a, double b, double atol = 1e-8, double rtol = 1e-5)
+    -> bool {
+    return std::abs(a - b) <= (atol + (rtol * std::abs(b)));
+}
+
+inline auto isclose3(const nbodysim::Vec3 &a, const nbodysim::Vec3 &b,
+                     double atol = 1e-8, double rtol = 1e-5) -> bool {
+    return isclose(a.v[0], b.v[0], atol, rtol) &&
+           isclose(a.v[1], b.v[1], atol, rtol) &&
+           isclose(a.v[2], b.v[2], atol, rtol);
+}
