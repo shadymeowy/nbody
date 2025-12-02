d1512c2f947380eb81d0bbf9902aed21091ddf70
shadymeowy <tolgademirdal@gmail.com>
2025-12-02 22:11:18 +0100

    euler integration scheme

---
+++ src/integrator/euler.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+// implements explicit euler integrator
+// generic over force calculation function (not force function itself for now)
+// simple but not very stable or accurate
+// instead see verlet.hpp for better integrator with similar performance
+
+#include <cstddef>
+#include <vector>
+
+#include "common/body.hpp"
+#include "integrator/simstate.hpp"
+
+namespace nbodysim {
+
+template <typename F>
+auto simulateEuler(std::vector<Body> &bodies, size_t n_steps, double dt,
+                   size_t output_interval, F &f) -> std::vector<SimState> {
+    // get number of bodies and steps
+    const size_t num_bodies = bodies.size();
+
+    // initialize simulation state
+    std::vector<SimState> states;
+    states.reserve(n_steps);
+
+    for (size_t block = 0; block < n_steps; block += output_interval) {
+        const double current_time = static_cast<double>(block) * dt;
+
+        // record current state
+        states.emplace_back(current_time, bodies);
+
+        // print progress
+        // TODO: use proper logging system
+        std::cout << "\rSimulated " << (block * 100) / n_steps << "% of steps."
+                  << std::flush;
+
+        for (size_t step = 0; step < output_interval; step++) {
+            // compute accelerations for each body
+            f(bodies);
+
+            // update velocities and positions (kick)
+            for (size_t i = 0; i < num_bodies; i++) {
+                auto &vel = bodies[i].vel;
+                auto &acc = bodies[i].acc;
+                auto &pos = bodies[i].pos;
+
+                // update velocity
+                vel.v[0] += acc.v[0] * dt;
+                vel.v[1] += acc.v[1] * dt;
+                vel.v[2] += acc.v[2] * dt;
+
+                // update position
+                pos.v[0] += vel.v[0] * dt;
+                pos.v[1] += vel.v[1] * dt;
+                pos.v[2] += vel.v[2] * dt;
+            }
+        }
+    }
+
+    // TODO: use proper logging system
+    std::cout << "\rSimulated 100% of steps.\n" << std::flush;
+
+    // return recorded states with RVO
+    return states;
+}
+
+}  // namespace nbodysim
